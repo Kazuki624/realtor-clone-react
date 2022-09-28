bdfbc04069cab2f5fd34409511a5faf177fdcf45
Kazuki624 <audi_kazuki0624@yahoo.co.jp>
2022-09-28 21:37:47 +0900

    complete the sign functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,34 +1,31 @@
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import {
-  doc,
-  getDoc,
-  serverTimestamp,
-  setDoc,
-  Timestamp,
-} from "firebase/firestore";
-import { db } from "../firebase";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { FcGoogle } from "react-icons/fc";
 import { toast } from "react-toastify";
-import { useNavigate } from "react-router";
-
-export const OAuth = () => {
-  const navigate = useNavigate("");
-  async function onClickGoogle() {
+import { db } from "../firebase";
+import { useNavigate } from "react-router-dom";
+export default function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick() {
     try {
       const auth = getAuth();
       const provider = new GoogleAuthProvider();
-      const result = await signInWithPopup(auth, provider); //googleでの登録を上記で行っている
+      const result = await signInWithPopup(auth, provider);
       const user = result.user;
 
-      const docRef = doc(db, "users", user.uid); //登録済みユーザの分別
-      const docSnap = await getDoc(docRef); //
+      // check for the user
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
       if (!docSnap.exists()) {
         await setDoc(docRef, {
           name: user.displayName,
           email: user.email,
-          Timestamp: serverTimestamp(),
+          timestamp: serverTimestamp(),
         });
       }
+
       navigate("/");
     } catch (error) {
       toast.error("Could not authorize with Google");
@@ -37,11 +34,11 @@ export const OAuth = () => {
   return (
     <button
       type="button"
-      onClick={onClickGoogle}
-      className="flex items-center justify-center bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium w-full hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-200 ease-in-out rounded"
+      onClick={onGoogleClick}
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
     >
-      <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
+      <FcGoogle className="text-2xl  bg-white rounded-full mr-2" />
       Continue with Google
     </button>
   );
-};
+}
