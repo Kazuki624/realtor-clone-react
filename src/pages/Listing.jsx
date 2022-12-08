206bacb672879dc49a6d6dcc3e25d665f3f6f81d
Kazuki624 <audi_kazuki0624@yahoo.co.jp>
2022-12-08 10:28:29 +0900

    add share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -8,11 +8,13 @@ import { db } from '../firebase';
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination} from "swiper";
 import "swiper/css/bundle";
+import { IoCopySharp } from 'react-icons/io5'
 
 export const Listing = () => {
      const params = useParams(); //パラメータ取得
      const [listing, setListing] = useState(null);
      const [loading, setLoading] = useState(true);
+     const [shareLinkCopy, setShareLinkCopy] = useState(false);
      SwiperCore.use([Autoplay, Navigation, Pagination]);
      useEffect(() => {
           const fetchListing = async()=> {
@@ -27,7 +29,7 @@ export const Listing = () => {
         }, [params.listingId]);
         if(loading){
           return <Spinner />
-        }
+     }
   return (
     <main>
      <Swiper
@@ -46,6 +48,19 @@ export const Listing = () => {
                </SwiperSlide>
           ))}
      </Swiper>
+     <div className='fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center'
+          onClick={() => {
+               navigator.clipboard.writeText(window.location.href);  //ドメインをコピーする
+               setShareLinkCopy(true);
+               setTimeout(() => {
+                    setShareLinkCopy(false)
+               },2000)
+          }}>
+          <IoCopySharp className='text-lg text-slate-500' />
+     </div>
+     {shareLinkCopy && (
+          <p className='fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2'>リンクをコピーしました</p>
+     )}
     </main>
   )
 }
