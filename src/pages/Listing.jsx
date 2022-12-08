ee94b43cf159f89d605a2c089a28bf58d68e365d
Kazuki624 <audi_kazuki0624@yahoo.co.jp>
2022-12-08 11:43:27 +0900

    add the listing information to the listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -8,8 +8,11 @@ import { db } from '../firebase';
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination} from "swiper";
 import "swiper/css/bundle";
-import { IoCopySharp } from 'react-icons/io5'
-
+import { IoCopySharp,IoBed } from 'react-icons/io5';
+import { ImLocation } from 'react-icons/im';
+import { FaBath, FaParking } from 'react-icons/fa' ;
+import { GiSofa } from 'react-icons/gi';
+ 
 export const Listing = () => {
      const params = useParams(); //パラメータ取得
      const [listing, setListing] = useState(null);
@@ -59,8 +62,70 @@ export const Listing = () => {
           <IoCopySharp className='text-lg text-slate-500' />
      </div>
      {shareLinkCopy && (
-          <p className='fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2'>リンクをコピーしました</p>
+          <p className='fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2'>
+               リンクをコピーしました
+          </p>
      )}
+     <div className=' m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5'>
+          <div className='w-full h-[250px] lg-[400px]'>
+               <p className='text-2xl font-bold mb-3 text-blue-900 '>
+                    {listing.name} - {listing.offer ? 
+                         listing.discountedPrice
+                         .toString()
+                         .replace(/\B(?=(\d{3})+(?!\d))/g, ",") : 
+                         listing.regularPrice
+                         .toString()
+                         .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                    }
+                    {listing.type === "rent" && " / 月 "}
+               </p>
+               <p className='flex items-center mt-6 mb-3 font-bold'>
+                    <ImLocation className='h-4 w-4 text-green-600 mr-1'/>
+                    {listing.address}
+               </p>
+               <div className='flex justify-start items-center space-x-4 w-[75%]'>
+                    <p className='bg-red-800 w-full max-w-[150px] rounded-md p-2 text-white text-center font-semibold shadow-md'>
+                         {listing.type === "rent" ? "Rent" : "Sale" }
+                    </p>
+                    {listing.offer && (
+                         <p className='w-full max-w-[300px] bg-green-800 rounded-md p-2 text-white text-center font-semibold shadow-md'>
+                              今なら
+                              {+listing.regularPrice - +listing.discountedPrice}
+                              万円で提供中！
+                         </p>
+                    )}
+               </div>
+               <p className='mt-3 mb-3'>
+                    <p className='font-semibold'>物件概要 - </p>
+                    {listing.description}
+               </p>
+               <ul className='flex items-center space-x-2 lg:space-x-10 text-sm font-semibold'>
+                    <li className=' flex items-center whitespace-nowrap'>
+                         <IoBed className='text-lg mr-1' />
+                         {+listing.bedrooms > 1 ? 
+                              `${listing.bedrooms} beds` : "1 bed"
+                         }
+                    </li>
+                    <li className=' flex items-center whitespace-nowrap'>
+                         <FaBath className='text-lg mr-1' />
+                         {+listing.bathrooms > 1 ? 
+                              `${listing.bathrooms} baths` : "1 bath"
+                         }
+                    </li>
+                    <li className=' flex items-center whitespace-nowrap'>
+                         <FaParking className='text-lg mr-1' />
+                         {+listing.parking  ? `Parking Spot` : "No Parking" }
+                    </li>
+                    <li className=' flex items-center whitespace-nowrap'>
+                         <GiSofa className='text-lg mr-1' />
+                         {+listing.furnished  ? `Furnished` : "Not Furnished" }
+                    </li>
+               </ul>
+          </div>
+          <div className='bg-blue-300 w-full h-[250px] lg-[400px] z-10 overflow-x-hidden'>
+
+          </div>
+     </div>
     </main>
   )
 }
